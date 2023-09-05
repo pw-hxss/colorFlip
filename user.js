ee7ea4388495e5900ac8a53e2d318ed0f37e5111
Hoshino Kenshi <124323263+pw-hxss@users.noreply.github.com>
2023-09-05 21:12:23 +0800

    Create user.js

---
+++ user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Invert Colors
+// @namespace    https://krunker.io/
+// @version      1.0
+// @description  反转颜色，在视频游戏中更容易识别敌人
+// @match        https://krunker.io/* //适用于所有网页
+// @grant        GM_addStyle
+// ==/UserScript==
+(function() {
+    var style = document.createElement('style');
+    style.textContent = '* { filter: invert(1) !important; } img, video { filter: invert(1) !important; }';
+    document.head.appendChild(style);
+})();
